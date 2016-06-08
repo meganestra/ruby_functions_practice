f191505983eaa6eb1784ca77a6bf05aaecac392e
Megan Strachan <m.e.strachan@gmail.com>
2016-06-08 09:30:27 +0100

    improve cube method

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -52,8 +52,8 @@ def number_to_short_month_name(number)
   end
 end
 
-def volume_of_cube(length, width, height)
-  return length * width * height
+def volume_of_cube(length)
+  return length**3
 end
 
 def volume_of_sphere(radius)
